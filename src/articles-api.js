aa7e27f75565522fd3b012569e72257c0351faeb
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-06-27 20:24:14 +0300

    1

--- src/articles-api.js
+++ src/articles-api.js
@@ -1,14 +1,33 @@
+// import axios from 'axios';
+// //https://jsonplaceholder.typicode.com
+// //https://65d889dac96fbb24c1bbbc49.mockapi.io/api/v1
+
+// // axios.defaults.baseURL = 'https://65d889dac96fbb24c1bbbc49.mockapi.io/api/v1';
+
+// axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+
+// const fetchArticles = async () => {
+//   const response = await axios.get('/posts');
+//   return response.data;
+// };
+
+// export default fetchArticles;
+
 import axios from 'axios';
-//https://jsonplaceholder.typicode.com
-//https://65d889dac96fbb24c1bbbc49.mockapi.io/api/v1
 
-// axios.defaults.baseURL = 'https://65d889dac96fbb24c1bbbc49.mockapi.io/api/v1';
+axios.defaults.baseURL = 'http://hn.algolia.com/api/v1';
 
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+const fetchArticles = async (searchQuery, currentPage) => {
+  // const response = await axios.get(`/search?query=${searchQuery}`);
 
-const fetchArticles = async () => {
-  const response = await axios.get('/posts');
-  return response.data;
+  const response = await axios.get('/search', {
+    params: {
+      query: searchQuery,
+      hitsPerPage: 10,
+      page: currentPage,
+    },
+  });
+  return response.data.hits;
 };
 
 export default fetchArticles;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from 'react';
 import fetchArticles from '../../articles-api';
 import ArticlesList from '../ArticlesList/ArticlesList';
+import SearchForm from '../SearchForm/SearchForm';
 
 export default function App() {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const [query, setQuery] = useState('');
+
+  const handleSearch = newQuery => {
+    setQuery(newQuery);
+  };
+
   useEffect(() => {
     async function getArticles() {
       try {
         setIsLoading(true);
-        const data = await fetchArticles();
+        const data = await fetchArticles(query);
         setError(false);
         setArticles(data);
       } catch (error) {
@@ -25,7 +33,7 @@ export default function App() {
   return (
     <div>
       <h1>HTTP requests</h1>
-
+      <SearchForm onSearch={handleSearch} />
       {error && <b>Error! Reload page!</b>}
 
       {isLoading && <b>Please wait, loading data...</b>}
