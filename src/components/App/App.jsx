d5bbf8253de87c975c8a4b33c9befb5dcbb94d5b
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-06-28 13:41:23 +0300

    delete import useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import fetchArticles from '../../articles-api';
 import ArticlesList from '../ArticlesList/ArticlesList';
 import SearchForm from '../SearchForm/SearchForm';
