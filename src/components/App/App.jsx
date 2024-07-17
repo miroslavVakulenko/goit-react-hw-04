dee83fbedf99db6dde9af6362b6d2eb6b2b69934
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-07-17 14:22:09 +0300

    add imports and base markup

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import SearchBar from '../SearchBar/SearchBar';
+import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
+import ImageGallery from '../ImageGallery/ImageGallery';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import ImageModal from '../ImageModal/ImageModal';
+export default function App() {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,12 @@
+import ImageCard from './ImageCard/ImageCard';
+
+export default function ImageGallery() {
+  return (
+    <ul>
+      {/* Набір елементів списку із зображеннями */}
+      <li>
+        <ImageCard />
+      </li>
+    </ul>
+  );
+}
